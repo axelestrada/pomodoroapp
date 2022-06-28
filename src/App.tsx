bb9d3f7944a65593c71a69f83e3239032a8fd4ed
Axel Estrada <axele1524@gmail.com>
2022-06-27 20:27:09 -0600

    Calculo manual del viewport height para evitar que la pagina se vea recortada

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Timers from "./components/Timers";
 import SettingsModal from "./components/SettingsModal";
@@ -9,9 +9,22 @@ import "./styles/main.sass";
 
 export default function App() {
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const [vh, setVh] = useState(window.innerHeight);
+
+  const updateVh = () => {
+    setVh(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateVh);
+
+    return () => {
+      window.removeEventListener("resize", updateVh);
+    };
+  }, []);
 
   return (
-    <main className="main">
+    <main className="main" style={{height: vh * 1}}>
       <h1 className="title">Pomodoro</h1>
 
       <Timers />
